86e0aa99a611353685316c0b069122460752726e
tengyou <2156396705@qq.com>
2023-08-25 22:48:04 +0800

    rename directories

--- homework1/build/cmake_install.cmake
+++ homework1/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/cs18/Desktop/homework/homework1
+# Install script for directory: /home/cs18/Desktop/homework/Games101Homework/homework2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,12 +37,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("/home/cs18/Desktop/homework/homework1/build/transformation/cmake_install.cmake")
-
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -51,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/cs18/Desktop/homework/homework1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/cs18/Desktop/homework/Games101Homework/homework2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
